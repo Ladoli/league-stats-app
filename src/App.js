cb0f451a51f9e04e960610bab309d89cd73c0f6e
Ladoli <villadolidangeloryan@gmail.com>
2018-11-20 22:04:13 -0800

    Main requirements and prettiness met

--- src/App.js
+++ src/App.js
@@ -2,196 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { map } from 'lodash';
 import './App.css';
-import { EFFICIENT_CHAMP_LIST, LEAGUE_SERVER } from './config';
+import { EFFICIENT_CHAMP_LIST, EFFICIENT_ITEM_LIST, EFFICIENT_RUNE_LIST, EFFICIENT_SPELL_LIST, LEAGUE_SERVER } from './config';
 
-let dummyData = { summonerName: 'C9 Sneaky',
-matches:
- [ { gameDuration: 2579,
-     participantId: 9,
-     championId: 516,
-     creepScore: 206,
-     champLevel: 18,
-     win: false,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 12,
-     primaryRune: 8300,
-     secondaryRune: 8400,
-     item0: 3193,
-     item1: 3083,
-     item2: 3025,
-     item3: 3194,
-     item4: 3047,
-     item5: 0,
-     item6: 3340 },
-   { gameDuration: 1872,
-     participantId: 4,
-     championId: 157,
-     creepScore: 155,
-     champLevel: 16,
-     win: false,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 11,
-     primaryRune: 8000,
-     secondaryRune: 8100,
-     item0: 1412,
-     item1: 3072,
-     item2: 3046,
-     item3: 3006,
-     item4: 3031,
-     item5: 3143,
-     item6: 3340 },
-   { gameDuration: 1473,
-     participantId: 1,
-     championId: 202,
-     creepScore: 187,
-     champLevel: 13,
-     win: true,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 7,
-     primaryRune: 8100,
-     secondaryRune: 8200,
-     item0: 1055,
-     item1: 3009,
-     item2: 3095,
-     item3: 3094,
-     item4: 2055,
-     item5: 1038,
-     item6: 3363 },
-   { gameDuration: 1840,
-     participantId: 8,
-     championId: 11,
-     creepScore: 231,
-     champLevel: 16,
-     win: true,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 3,
-     primaryRune: 8000,
-     secondaryRune: 8100,
-     item0: 3153,
-     item1: 3124,
-     item2: 3508,
-     item3: 1054,
-     item4: 3111,
-     item5: 2055,
-     item6: 3340 },
-   { gameDuration: 1403,
-     participantId: 2,
-     championId: 29,
-     creepScore: 164,
-     champLevel: 12,
-     win: false,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 7,
-     primaryRune: 8000,
-     secondaryRune: 8200,
-     item0: 3153,
-     item1: 3085,
-     item2: 1055,
-     item3: 3047,
-     item4: 1037,
-     item5: 1038,
-     item6: 3363 },
-   { gameDuration: 2067,
-     participantId: 10,
-     championId: 126,
-     creepScore: 174,
-     champLevel: 17,
-     win: false,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 3,
-     primaryRune: 8200,
-     secondaryRune: 8100,
-     item0: 3814,
-     item1: 3142,
-     item2: 3147,
-     item3: 3047,
-     item4: 3071,
-     item5: 1053,
-     item6: 3363 },
-   { gameDuration: 1296,
-     participantId: 9,
-     championId: 81,
-     creepScore: 178,
-     champLevel: 11,
-     win: false,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 12,
-     primaryRune: 8300,
-     secondaryRune: 8200,
-     item0: 1055,
-     item1: 3042,
-     item2: 3078,
-     item3: 0,
-     item4: 0,
-     item5: 2422,
-     item6: 3340 },
-   { gameDuration: 1570,
-     participantId: 4,
-     championId: 238,
-     creepScore: 148,
-     champLevel: 13,
-     win: false,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 14,
-     primaryRune: 8100,
-     secondaryRune: 8200,
-     item0: 3147,
-     item1: 3142,
-     item2: 3047,
-     item3: 3156,
-     item4: 2031,
-     item5: 2055,
-     item6: 3340 },
-   { gameDuration: 1361,
-     participantId: 4,
-     championId: 238,
-     creepScore: 148,
-     champLevel: 13,
-     win: true,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 14,
-     primaryRune: 8100,
-     secondaryRune: 8200,
-     item0: 3147,
-     item1: 3814,
-     item2: 3142,
-     item3: 3047,
-     item4: 1036,
-     item5: 2055,
-     item6: 3340 },
-   { gameDuration: 1585,
-     participantId: 8,
-     championId: 103,
-     creepScore: 184,
-     champLevel: 15,
-     win: true,
-     kda: [Object],
-     spell1Id: 4,
-     spell2Id: 7,
-     primaryRune: 8100,
-     secondaryRune: 8300,
-     item0: 1056,
-     item1: 3285,
-     item2: 1082,
-     item3: 3020,
-     item4: 3165,
-     item5: 1058,
-     item6: 3340 } ],
-accountID: 78247 };
+
 
 class App extends Component {
   state = {
-    matchInfo: dummyData,
-    summonerName: null
+    matchInfo: null,
+    summonerName: ""
   }
 
   handleSubmit = () => {
@@ -245,15 +63,94 @@ class App extends Component {
               let gameMinutes = (value.gameDuration/60).toFixed(0);
               let gameSeconds = value.gameDuration%60;
               let championName = EFFICIENT_CHAMP_LIST[value.championId].name;
+              let championNameImageFormat = championName.replace(/[^A-Z0-9]/ig, '').toLowerCase();
+              championNameImageFormat = championNameImageFormat.charAt(0).toUpperCase() + championNameImageFormat.slice(1);
+              let championImageLoc = championNameImageFormat + ".png";
+              let spell1Name = EFFICIENT_SPELL_LIST[value.spell1Id].name;
+              let spell2Name = EFFICIENT_SPELL_LIST[value.spell2Id].name;
+              let primaryRuneName = value.primaryRune ? EFFICIENT_RUNE_LIST[value.primaryRune].name : "";
+              let secondaryRuneName = value.secondaryRune ? EFFICIENT_RUNE_LIST[value.secondaryRune].name : "";
+              
+              console.log(value.item6)
+
+
+              let items = {
+                0: value.item0 ?  EFFICIENT_ITEM_LIST[value.item0] ? EFFICIENT_ITEM_LIST[value.item0].name : "" : "",
+                1: value.item1 ?  EFFICIENT_ITEM_LIST[value.item1] ? EFFICIENT_ITEM_LIST[value.item1].name : "" : "",
+                2: value.item2 ?  EFFICIENT_ITEM_LIST[value.item2] ? EFFICIENT_ITEM_LIST[value.item2].name : "" : "",
+                3: value.item3 ?  EFFICIENT_ITEM_LIST[value.item3] ? EFFICIENT_ITEM_LIST[value.item3].name : "" : "",
+                4: value.item4 ?  EFFICIENT_ITEM_LIST[value.item4] ? EFFICIENT_ITEM_LIST[value.item4].name : "" : "",
+                5: value.item5 ?  EFFICIENT_ITEM_LIST[value.item5] ? EFFICIENT_ITEM_LIST[value.item5].name : "" : "",
+                6: value.item6 ?  EFFICIENT_ITEM_LIST[value.item6] ? EFFICIENT_ITEM_LIST[value.item6].name : "" : "",
+              }
+
+
+              let creepsPerMin = (value.creepScore/(value.gameDuration/60)).toFixed(1);
               return (
-                <div key={key} className={matchClass + " matchTable"}>
-                  <div className="matchState">
-                    {victoryStatus} 
-                    <br />
-                    {gameMinutes + "m " + gameSeconds + "s"} 
-                  </div>
-                  <div>
-                    {championName}
+                <div key={key} className="matchTable">
+                  <div className={matchClass + " matchContainer"} style={{minWidth: "320px", display: "inline-block"}}>
+                    <div className="matchState">
+                      {victoryStatus +  " - " + gameMinutes + "m " + gameSeconds + "s"} 
+                    </div>
+                    <div className="champStats">
+                      <img alt={championName} className="championImage" src={process.env.PUBLIC_URL + "/img/champion/" + championImageLoc} />
+                      <div className="champTextStats">
+                        <div className="runesAndSpells">
+                          <div style={{padding: "10px"}}>
+                            {spell1Name}
+                            <br/>
+                            {spell2Name}
+                          </div>
+                          <div style={{color: "rgb(255,140,0)", padding: "10px"}}>
+                            {primaryRuneName}
+                            <br/>
+                            {secondaryRuneName}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="kdaStats">
+                      <div>
+
+                        <div className="championName">
+                          {championName} <span style={{color: "green", fontSize: ".7em"}}>{" - Lvl: " + value.champLevel}</span>  
+                        </div>
+                        <br />
+                        <div>
+                          <span 
+                            style={{color:"green"}}>
+                            {value.kda.kills}
+                          </span>
+                          <span 
+                            style={{color:"black"}}>{"/"}
+                          </span>
+                          <span
+                            style={{color:"red"}}>{value.kda.deaths}
+                          </span>
+                          <span 
+                            style={{color:"black"}}>{"/"}
+                          </span>                    
+                          <span
+                            style={{color:"blue"}}>{value.kda.assists}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <br/>
+                    <div className="itemList">
+                      {
+                        map(items, (value,key)=>{
+                          return (
+                            <div key={key}>{value}</div>
+                          )
+                        })
+                      }
+                    </div>
+                    <div className="creepStats">
+                      Total Creeps: {value.creepScore}
+                      <br />
+                      Creeps Per Minute: {creepsPerMin}
+                    </div>
                   </div>
                 </div>
               )
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,23 @@
 import championList from 'lol-champions';
-import { keyBy } from 'lodash';
+import spellList from 'lol-spells';
+// import itemList from 'lol-items/items';
+import { keyBy, map } from 'lodash';
+import runeList from '../json/runesReforged.json'; 
+import itemList from '../json/item.json'; 
+
+
 export const EFFICIENT_CHAMP_LIST = keyBy(championList, 'key');
+export const EFFICIENT_ITEM_LIST = keyBy(map(itemList.data, (value,key)=>{
+    return {
+        name: value.name,
+        id: key
+    }
+}),'id');
+export const EFFICIENT_RUNE_LIST = keyBy(map(runeList, (value,key)=>{
+    return {
+        name: value.name,
+        id: value.id
+    }
+}),'id');
+export const EFFICIENT_SPELL_LIST = keyBy(spellList, 'key');
 export const LEAGUE_SERVER = "http://localhost:5000/";
